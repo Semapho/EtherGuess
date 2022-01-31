ad08fe555af16e00ce27024f8e57060aa51c963b
lhxsimon <lhxsimon@gmail.com>
2022-02-01 00:10:57 +0800

    fix panic bug

--- internal/guess/utils.go
+++ internal/guess/utils.go
@@ -11,22 +11,18 @@ import (
 	"sync/atomic"
 )
 
-func (c *Client) generateAccount() (string, string) {
+func (c *Client) generateAccount() (string, string, error) {
 	prvKey, err := crypto.GenerateKey()
 	if err != nil {
-		panic(err)
+		return "", "", err
 	}
 	prvData := crypto.FromECDSA(prvKey)
 	pubAddress := crypto.PubkeyToAddress(prvKey.PublicKey).Hex()
-	return hexutil.Encode(prvData), pubAddress
+	return hexutil.Encode(prvData), pubAddress, nil
 }
 
-func (c *Client) getBalance(address string) *big.Int {
-	balance, err := c.conn.BalanceAt(context.Background(), common.HexToAddress(address), nil)
-	if err != nil {
-		panic(err)
-	}
-	return balance
+func (c *Client) getBalance(address string) (*big.Int, error) {
+	return c.conn.BalanceAt(context.Background(), common.HexToAddress(address), nil)
 }
 
 func (c *Client) parseEther(data *big.Int) *big.Float {
@@ -38,9 +34,17 @@ func (c *Client) parseEther(data *big.Int) *big.Float {
 
 func (c *Client) guessEther(finishChan chan<- bool) {
 	for {
-		privateKey, address := c.generateAccount()
+		privateKey, address, err := c.generateAccount()
+		if err != nil {
+			log.Errorf("generate account error: %v", err)
+			continue
+		}
+		balance, err := c.getBalance(address)
+		if err != nil {
+			log.Errorf("get balance error: %v", err)
+			continue
+		}
 		atomic.AddUint64(&c.total, 1)
-		balance := c.getBalance(address)
 		if balance.Cmp(big.NewInt(0)) == 1 {
 			etherBalance := c.parseEther(balance)
 			log.Infof("find account: %s, private key: %s, balance: %v\n", address, privateKey, etherBalance)
